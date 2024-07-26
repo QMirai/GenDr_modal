25d472a4968bbd7032ce75083ccb8e4844b19ad8
QMirai <smallqiao4@outlook.com>
2024-07-25 21:25:37 -0400

    sandwich

--- bin/test-win.bat
+++ bin/test-win.bat
@@ -1,1 +1,1 @@
-java -Dencoding=UTF-8 -Dlng=fre -Duser.home=. -Dstrname=test-eval -Droot=D:\cours\memoire\GenDR\branches\modals -jar ..\..\..\mate.jar -exec .\test-win.prg > test.log
+java -Dencoding=UTF-8 -Dlng=fre -Duser.home=. -Dstrname=/test-fre-dummy/prg-2/all.str -Droot=D:\cours\memoire\GenDR\branches\modals -jar ..\..\..\mate.jar -exec .\test-win.prg > test.log
